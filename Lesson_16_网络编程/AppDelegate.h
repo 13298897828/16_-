b686c7ec8fe769f01db3abda8412d66d056090ae
Tianqi Zhang <1120404010@qq.com>
2015-09-22 10:42:06 +0800

    Initial Commit

---
+++ Lesson_16_网络编程/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Lesson_16_网络编程
+//
+//  Created by 张天琦 on 15/9/22.
+//  Copyright (c) 2015年 张天琦. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Lesson_16_网络编程/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Lesson_16_网络编程
+//
+//  Created by 张天琦 on 15/9/22.
+//  Copyright (c) 2015年 张天琦. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
